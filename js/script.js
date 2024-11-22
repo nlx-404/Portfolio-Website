e5aff7f94a384298851b3fd52f0a0778e8b3b4ec
nlx-404 <ardeleannoel@gmail.com>
2024-11-22 12:49:15 +0000

    Added boot-up screen

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Clock function in header
 function updateTime() {
     let dt = new Date();
     document.getElementById("datetime").innerHTML = dt.toLocaleString([], {
@@ -12,3 +13,19 @@ function updateTime() {
 setInterval(updateTime, 1000);
 
 updateTime();
+
+// Boot-up screen
+
+// Disable scrolling during boot up screen
+document.body.style.overflow = 'hidden';
+
+// Add hide to it and let you scroll again
+setTimeout(() => {
+    document.querySelector('.boot-screen').classList.add('hide');
+    document.body.style.overflow = 'auto';
+}, 2300);
+
+// Remove the boot screen from the DOM
+setTimeout(() => {
+    document.querySelector('.boot-screen').remove();
+},3300);
